f837810f6c0c05402cb9228c345c01d04dc8641d
mr190e <84240334+mr190e@users.noreply.github.com>
2023-05-08 22:14:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const Discord = require('discord.js');
-const config = require('./config.json');
+
+// Load the config file
+let config;
+try {
+  config = require('./config.json');
+} catch (error) {
+  console.error('Failed to load config file:', error);
+  process.exit(1);
+}
 
 // Set up the Discord client
 const client = new Discord.Client({
@@ -15,41 +23,32 @@ const client = new Discord.Client({
 const directoryPath = config.path;
 
 // Set the channel to send the message to
-const channelID = config.channel;
+const channelID = config.channelID;
 
-// Watch the directory recursively for new files with the extension specified in the config
+// Set the file extension to monitor
+const fileExtension = config.fileExtension;
+
+// Watch the directory recursively for new files and folders with the specified extension
 fs.watch(directoryPath, { recursive: true }, (eventType, filename) => {
-  if (filename && eventType === 'rename' && path.extname(filename) === config.extension) {
+  if (filename && eventType === 'rename' && path.extname(filename) === `.${fileExtension}`) {
     const renamedFilename = `${path.parse(filename).name}.txt`;
-    fs.renameSync(path.join(directoryPath, filename), path.join(directoryPath, renamedFilename));
-    const attachment = new Discord.MessageAttachment(path.join(directoryPath, renamedFilename));
-    const embedMessage = new Discord.MessageEmbed()
-      .setTitle('New file uploaded')
-      .setColor('#00FF00')
-      .setDescription(`File ${filename} has been renamed to ${renamedFilename} and uploaded!`);
-    client.channels.cache.get(channelID).send({ embeds: [embedMessage], files: [attachment] })
-      .then(() => console.log(`File ${renamedFilename} sent to channel ${channelID}!`))
-      .catch(console.error);
-  }
-});
-
-// Watch the directory recursively for new folders with files with the extension specified in the config
-fs.watch(directoryPath, { recursive: true }, (eventType, filename) => {
-  if (eventType === 'rename' && fs.existsSync(path.join(directoryPath, filename)) && fs.lstatSync(path.join(directoryPath, filename)).isDirectory()) {
-    const files = fs.readdirSync(path.join(directoryPath, filename));
-    const admFiles = files.filter(file => path.extname(file) === config.extension);
-    if (admFiles.length > 0) {
-      const attachment = admFiles.map(file => new Discord.MessageAttachment(path.join(directoryPath, filename, file)));
+    fs.rename(path.join(directoryPath, filename), path.join(directoryPath, renamedFilename), (err) => {
+      if (err) {
+        console.error(`Failed to rename file ${filename}:`, err);
+        return;
+      }
+      const attachment = new Discord.MessageAttachment(path.join(directoryPath, renamedFilename));
       const embedMessage = new Discord.MessageEmbed()
-        .setTitle('New folder uploaded')
+        .setTitle('New file uploaded')
         .setColor('#00FF00')
-        .setDescription(`Folder ${filename} has been uploaded!`);
-      client.channels.cache.get(channelID).send({ embeds: [embedMessage], files: attachment })
-        .then(() => console.log(`Folder ${filename} sent to channel ${channelID}!`))
+        .setDescription(`File ${filename} has been renamed to ${renamedFilename} and uploaded!`)
+      client.channels.cache.get(channelID).send({ embeds: [embedMessage], files: [attachment] })
+        .then(() => console.log(`File ${renamedFilename} sent to channel ${channelID}!`))
         .catch(console.error);
-    }
+    });
   }
 });
 
 // Log in to Discord
-client.login(config.token);
+const token = config.token;
+client.login(token);
